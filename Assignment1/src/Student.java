e37eb117ac00d0b3516cfca8a92b91a728c79167
Liskuxx <lelea500@gmail.com>
2020-07-11 03:34:58 +1200

    Student class created

---
+++ Assignment1/src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+	//Declare variables, will all be public until tested and maybe will be altered to private
+	public int id;
+	public String firstName;
+	public String lastName;
+	public AssignmentMarks mathMarks;
+	public AssignmentMarks englishMarks;
+	
+	//Methods
+	
+	//Constructor
+	public Student(int ID, String FirstName, String LastName) {
+		//The variables are capitalized in here to differentiate from the first time they are used above
+		id = ID;
+		firstName = FirstName;
+		lastName = LastName;
+	}
+	
+	//Return full name
+	public String getFullName() {
+		String fullName = firstName + " " + lastName;
+		return fullName;
+	}
+}
